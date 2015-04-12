77f6dd6744cd0db6ddddc0ba9f8f9c9043d54ad5
Bryan Robbins <bryantrobbins@gmail.com>
2015-04-12 00:01:59 -0400

    Initial commit

---
+++ src/main/groovy/com/btr3/cool/SomethingCoolApplication.groovy
@@ -0,0 +1,12 @@
+package com.btr3.cool
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SomethingCoolApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run SomethingCoolApplication, args
+    }
+}
---
+++ src/test/groovy/com/btr3/cool/SomethingCoolApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.btr3.cool
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+@RunWith(SpringJUnit4ClassRunner)
+@SpringApplicationConfiguration(classes = SomethingCoolApplication)
+class SomethingCoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
